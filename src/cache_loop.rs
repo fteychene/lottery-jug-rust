f05f93f3eb23ec0430377f321a813adb5277ca3f
fteychene <francois.teychene@gmail.com>
2018-11-04 00:58:26 +0100

    Step 7 : Logs

--- src/cache_loop.rs
+++ src/cache_loop.rs
@@ -11,13 +11,13 @@ use lotterycache::{LotteryCache, UpdateAttendees, UpdateAttendeesResponse};
 pub fn cache_update_interval(duration: u64, addr: Addr<LotteryCache>, token: String, organizer: String) -> impl Future<Item=(), Error=()> + 'static {
     Interval::new(Instant::now(), Duration::from_secs(duration))
         .then(move |_instant| addr.send(UpdateAttendees { token: token.clone(), organizer: organizer.clone() })
-            .map_err(|err| eprintln!("Error on sending update message : {:?}", err)))
+            .map_err(|err| error!("Error on sending update message : {:?}", err)))
         .for_each(move |res| {
             match res {
-                UpdateAttendeesResponse::Updated => println!("Attendees cache updated"),
-                UpdateAttendeesResponse::NoEventAvailable => println!("No event available on eventbrite"),
-                UpdateAttendeesResponse::EventbriteError { error: ref e } => println!("Error on eventbrite : {}", e),
-                UpdateAttendeesResponse::UnexpectedError { error: ref e } => eprintln!("Unexpected error on update attendees \n{:?}", e)
+                UpdateAttendeesResponse::Updated => info!("Attendees cache updated"),
+                UpdateAttendeesResponse::NoEventAvailable => info!("No event available on eventbrite"),
+                UpdateAttendeesResponse::EventbriteError { error: ref e } => info!("Error on eventbrite : {}", e),
+                UpdateAttendeesResponse::UnexpectedError { error: ref e } => error!("Unexpected error on update attendees \n{:?}", e)
             };
             Ok(())
         })
